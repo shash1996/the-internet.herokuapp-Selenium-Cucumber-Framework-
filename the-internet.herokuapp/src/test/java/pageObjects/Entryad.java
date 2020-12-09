d2666ffe9f3ea181a52a436c526e23fa4054088a
shashwat <sha97katiyar@gmail.com>
2020-12-10 00:15:59 +0530

    DAY-2--ADDED BASIC FRAMEWORK STRUCTURE

--- the-internet.herokuapp/src/test/java/pageObjects/Entryad.java
+++ the-internet.herokuapp/src/test/java/pageObjects/Entryad.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 
 import resources.FunctionalUtilities;
 
-public class Entryad extends FunctionalUtilities{
+public class Entryad extends FunctionalUtilities {
 	
 	WebDriver driver;
 	public void hitUrl() throws IOException {
